1d0e322d3331d131682491a231a4cd1a7a5f8814
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-22 12:32:43 +0100

    Update MainVolador.java
    Introduccion de comentarios

--- src/MainVolador.java
+++ src/MainVolador.java
@@ -2,7 +2,7 @@ public class MainVolador {
     public static void main(String[] args) {
         Volar ave = new Ave();
         Volar avion = new Avion();
-
+//Introduccion de clases con la interfaz volar
         ave.volar();
         ave.aterrizar();
         System.out.println("===============================");
