1fcf96dc734a213e0a64a4532a35c1f6d39ea5e2
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-22 12:31:22 +0100

    Update Avion.java
    Introduccion de comentarios

--- src/Avion.java
+++ src/Avion.java
@@ -1,8 +1,11 @@
+//Creamos una clase Ave que esta unida ala interfaz de volar
 public class Avion implements Volar {
+    //Creamos el metodo aterrizar 
     @Override
     public void aterrizar() {
         System.out.println("Avion aterrizando");
     }
+    //Creamos el metodo voalr 
     @Override
     public void volar() {
         System.out.println("Avion volar");
