9961a05f791a3e085ff9f68a1daf4197fb287742
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-22 12:29:38 +0100

    Update Ave.java
    introduccion de datos

--- src/Ave.java
+++ src/Ave.java
@@ -1,10 +1,11 @@
+//Creamos una clase Ave que esta unida ala interfaz de volar
 public class Ave implements Volar {
-
+//Creamos el metodo aterrizar 
     @Override
     public void aterrizar() {
         System.out.println("Ave aterrizando");
     }
-
+//Creamos el metodo volar
     @Override
     public void volar() {
         System.out.println("Ave volar");
